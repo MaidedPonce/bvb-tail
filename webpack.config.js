8f8c796440c3016edb5a0a3425529aaab6f5ee6b
Maided Ponce <maided.h.p90@gmail.com>
2021-11-23 21:32:50 -0600

    añadiendo estilos e imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/"
   },
   mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+      "@logos": path.resolve(__dirname, "src/assets/logos")
+    }
   },
   module: {
     rules: [
@@ -27,6 +38,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svs|jpg|gif)$/,
+        type: "asset"
+      }
     ],
   },
   plugins: [
@@ -34,5 +53,16 @@ module.exports = {
       template: "./public/index.html",
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
   ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8000,
+  },
 };
